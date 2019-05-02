dbfa8d970d11eaeabe7cad7b4dc22cb27cfc63fc
David Korff <dkorff12@gmail.com>
2019-05-02 11:38:06 -0400

    Added cheers and jeers counts to serialized data for trumpets

--- app/serializers/api/v1/trumpet_serializer.rb
+++ app/serializers/api/v1/trumpet_serializer.rb
@@ -1,7 +1,19 @@
 class Api::V1::TrumpetSerializer < ActiveModel::Serializer
-  attributes :id, :trumpet_type, :content
+  attributes :id, :trumpet_type, :content, :cheers, :jeers
 
   belongs_to :user
   belongs_to :website
   has_many :reactions
+
+  def cheers
+    count = 0
+    self.object.reactions.each {|reaction| count += 1 if reaction.cheer }
+    count
+  end
+
+  def jeers
+    count = 0
+    self.object.reactions.each {|reaction| count += 1 if reaction.jeer }
+    count
+  end
 end
