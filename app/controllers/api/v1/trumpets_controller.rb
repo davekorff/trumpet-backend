417fabd07d5b80acfc387487f1e497a4f1258fea
David Korff <dkorff12@gmail.com>
2019-05-02 16:26:28 -0400

    Added create and update trumpet routes and controller actions

--- app/controllers/api/v1/trumpets_controller.rb
+++ app/controllers/api/v1/trumpets_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TrumpetsController < ApplicationController
-  before_action :find_trumpet, only: [:show]
+  before_action :find_trumpet, only: [:show, :update]
 
   def index
     @trumpets = Trumpet.all
@@ -10,7 +10,23 @@ class Api::V1::TrumpetsController < ApplicationController
     render json: @trumpet
   end
 
+  def create
+    @website = Website.find_or_create_by(url: trumpet_params[:url], root_url: trumpet_params[:root_url])
+    @trumpet = Trumpet.create!(user_id: trumpet_params[:user_id], website_id: @website.id, summary: trumpet_params[:summary], trumpet_type: trumpet_params[:trumpet_type], content: trumpet_params[:content])
+    json_response(@trumpet, :created)
+  end
+
+  def update
+    @website = Website.find_or_create_by(url: trumpet_params[:url], root_url: trumpet_params[:root_url])
+    @trumpet.update(user_id: trumpet_params[:user_id], website_id: @website.id, summary: trumpet_params[:summary], trumpet_type: trumpet_params[:trumpet_type], content: trumpet_params[:content])
+    head :no_content
+  end
+
   private
+
+  def trumpet_params
+    params.permit(:user_id, :url, :root_url, :summary, :trumpet_type, :content)
+  end
 
   def find_trumpet
     @trumpet = Trumpet.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :trumpets, only: [:index, :show]
+      resources :trumpets, only: [:index, :show, :create, :update]
       resources :users, only: [:index, :show]
     end
   end
