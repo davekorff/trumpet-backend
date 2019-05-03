27e57bd2f02ba2d62f16e52ed82283d71dae5d25
David Korff <dkorff12@gmail.com>
2019-05-03 16:09:27 -0400

    Added password to api

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name
+  attributes :id, :username, :first_name, :last_name, :password
 
   has_many :trumpets
   has_many :reactions
